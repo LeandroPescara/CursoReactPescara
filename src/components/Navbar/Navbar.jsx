cc2317e2fb43842f58bf06b5bd17f9f4237bf2f9
LeandroPescara <041094344lp@gmail.com>
2024-03-04 22:02:37 -0300

    se agrega el proyecto

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <nav className="ContainerNav">
+      <div>⚡</div>
+      <ul>
+        <li>2 puertas</li>
+        <li>4 puertas</li>
+        <li>todo terreno</li>
+      </ul>
+      <div>🛒 5</div>
+    </nav>
+  );
+};
